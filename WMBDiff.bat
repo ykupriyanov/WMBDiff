bc416bfdc763b8154953e49327a54b1f25be989c
ykupriyanov <yaroslav.kupriyanov@gmail.com>
2015-06-28 09:56:04 +0300

    Add log settings and modified WMBDiff.bat

--- WMBDiff.bat
+++ WMBDiff.bat
@@ -1,8 +1,11 @@
-set CLASSPATH="C:\Program Files (x86)\IBM\WebSphere MQ\java\lib\com.ibm.mq.jmqi.jar;C:\Program Files (x86)\IBM\WebSphere MQ\java\lib\com.ibm.mq.jar"
-set CLASSPATH="C:\Program Files\IBM\MQSI\7.0\classes\ConfigManagerProxy.jar";%CLASSPATH%
-set CLASSPATH=./target/wmbdiff-1.0.0-jar-with-dependencies.jar;%CLASSPATH%
+set IBM_JRE6_PATH="C:\Program Files (x86)\IBM\WebSphere MQ\java\jre\bin\java"
+rem IBM_JRE6_PATH="C:\Program Files\IBM\MQSI\7.0\jre16\bin\java"
+set "MQ_JAVA_LIB_PATH=C:\Program Files (x86)\IBM\WebSphere MQ\java\lib"
+set "MQSI_CMP_JAR=C:\Program Files\IBM\MQSI\7.0\classes\ConfigManagerProxy.jar"
+set CLASSPATH=%MQ_JAVA_LIB_PATH%\com.ibm.mq.jmqi.jar;%MQ_JAVA_LIB_PATH%\com.ibm.mq.jar
+set CLASSPATH=%MQSI_CMP_JAR%;%CLASSPATH%
+set CLASSPATH=.;./target/wmbdiff-1.0.0-jar-with-dependencies.jar;%CLASSPATH%
 
-rem "C:\Program Files\IBM\MQSI\7.0\jre16\bin\java" -cp %CLASSPATH% ru.wmbdiff.WMBDiff
-"C:\Program Files (x86)\IBM\WebSphere MQ\java\jre\bin\java" -cp %CLASSPATH% ru.wmbdiff.WMBDiff
+%IBM_JRE6_PATH% -cp "%CLASSPATH%" ru.wmbdiff.WMBDiff
 
 
